f3bd2b74c7c209b5aeba250cd9e047ea9e58928e
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2023-02-04 12:25:33 +0800

    chore: custom

--- .vitepress/theme/components/Banner.vue
+++
@@ -1,6 +0,0 @@
-<script setup>
-import { onMounted } from "vue"
-onMounted(()=>{
-    document.getElementsByClassName("VPNavBarTitle")[0].innerHTML = `<span class="text">Hexo Theme Nexmoe</span>`;
-})
-</script>
